3ae55ecfd568d42a1317e8acd814b2197c02c4ff
Justin Moon <mail@justinmoon.com>
2019-02-20 12:24:06 -0600

    Finish Lesson 2

--- 2. Reading Version Messages/utils.py
+++ 2. Reading Version Messages/utils.py
@@ -3,3 +3,12 @@ def replace_bytes(base, index, new_bytes):
     prefix = base[:index]
     suffix = base[index+len(new_bytes):]
     return prefix + new_bytes + suffix
+
+def assert_len(payload, field, expected_len):
+    observed_len = len(payload[field])
+    assert observed_len == expected_len,\
+        f'The "{field}" field should be {expected_len} bytes, was {observed_len} bytes'
+
+def check_field(payload, field, bytes_value, int_value):
+    assert payload[field] == int_value,\
+        f'Correct integer interpretation of {bytes_value} for field "{field}" is {int_value}, not {payload[field]}'
